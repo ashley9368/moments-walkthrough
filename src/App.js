920559b020cff2333919ee1bd3dd1b9c3e0efc82
ashley9368 <ashleygoward@icloud.com>
2025-02-28 16:40:44 +0000

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <NavBar />
     </div>
   );
 }

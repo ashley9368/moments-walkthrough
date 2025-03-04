3a9f68557549abe78dda288bec56eba9ebaaf315
ashley9368 <ashleygoward@icloud.com>
2025-03-04 15:16:10 +0000

    update axios

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "drf-api-walkthrough-1.herokuapp.com";
+axios.defaults.baseURL = "https://drf-api-walkthrough-1-b4e76842989a.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
